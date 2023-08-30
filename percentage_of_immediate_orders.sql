1919ab621d4a3fc03fa970cb64fbf31b782b80b4
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2023-08-30 15:28:29 -0600

    Create percentage_of_immediate_orders.sql

---
+++ percentage_of_immediate_orders.sql
@@ -0,0 +1,14 @@
+-- If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+
+-- The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+
+-- Write an SQL query to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+
+Select 
+    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
