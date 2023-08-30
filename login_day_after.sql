5fc77096b6457468d3a58f0f75b63730c43d9ea3
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2023-08-30 16:06:46 -0600

    Create login_day_after.sql

---
+++ login_day_after.sql
@@ -0,0 +1,11 @@
+-- Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+
+SELECT
+  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM
+  Activity
+WHERE
+  (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+  IN (
+    SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
+  );
