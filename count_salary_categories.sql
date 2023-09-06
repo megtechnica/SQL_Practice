d775571690bacdb988e0bd7399cb439fe083ce37
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2023-09-06 15:16:07 -0600

    Create count_salary_categories.sql

---
+++ count_salary_categories.sql
@@ -0,0 +1,22 @@
+-- Write a solution to calculate the number of bank accounts for each salary category. The salary categories are:
+
+-- "Low Salary": All the salaries strictly less than $20000.
+-- "Average Salary": All the salaries in the inclusive range [$20000, $50000].
+-- "High Salary": All the salaries strictly greater than $50000.
+-- The result table must contain all three categories. If there are no accounts in a category, return 0.
+
+SELECT "Low Salary" AS category,
+       sum(income < 20000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "Average Salary" AS category,
+       sum(income BETWEEN 20000 AND 50000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "High Salary" AS category,
+       sum(income > 50000) AS accounts_count
+  FROM Accounts;
